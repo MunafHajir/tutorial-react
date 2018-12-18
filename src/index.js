3a5e3a4fc042598e78e05c7711347a5351e52cef
Munaf Hajir <munafhajirpro@gmail.com>
2018-12-18 12:25:39 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// stateless functional component
+// always return jsx
+function Greeting() {
+    return ( 
+    <div> Hello React </div>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render( <Greeting /> , document.getElementById("root")
+);
